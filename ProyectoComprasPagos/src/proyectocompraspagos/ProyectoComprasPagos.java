88822273c23e7262d68eb3d46a04ea79803aa47c
GustavoSistemasPotigian <soporte2@potigian.com.ar>
2017-09-11 02:41:43 -0300

    ProyectoComprasPagos.java. form

--- ProyectoComprasPagos/src/proyectocompraspagos/ProyectoComprasPagos.java
+++ ProyectoComprasPagos/src/proyectocompraspagos/ProyectoComprasPagos.java
@@ -15,7 +15,11 @@ public class ProyectoComprasPagos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+     
+        
+// TODO code application logic here
+
+
     }
     
 }
