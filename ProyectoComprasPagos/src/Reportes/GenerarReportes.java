18a1a87a3907ee2cf5ee7c737d2a0fb0da599f9e
GustavoSistemasPotigian <soporte2@potigian.com.ar>
2017-09-19 21:53:42 -0300

    GenerarReportes.java 19/09/2017

--- ProyectoComprasPagos/src/Reportes/GenerarReportes.java
+++ ProyectoComprasPagos/src/Reportes/GenerarReportes.java
@@ -6,6 +6,7 @@
 package Reportes;
 
 import BaseDeDatos.ConexionMySQL;
+import Modelos.ConsultaPlanes;
 import java.sql.Connection;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,22 +22,27 @@ import net.sf.jasperreports.view.JasperViewer;
  * @author gustavo
  */
 public class GenerarReportes {
-    
-    public void reportePlanFijo(String valor)
+    ConexionMySQL mysql= new ConexionMySQL();
+            Connection cn= mysql.Conectar();
+    public void reportePlanFijo(int idPlanFijo)
     {
-        try {
             
+        
+          try {
+            System.out.println("entra5");
             JasperReport reporte= (JasperReport) JRLoader.loadObject("ReportePlanFijo.jasper");
             Map parametro= new HashMap();
-            parametro.put("valor", valor);
-            ConexionMySQL mysql= new ConexionMySQL();
-            Connection cn= mysql.Conectar();
+            parametro.put("idPlanFijo", idPlanFijo);
+            System.out.println("entra6");
             
             JasperPrint j= JasperFillManager.fillReport(reporte, parametro,cn);
+             System.out.println("entra7");
             JasperViewer jv= new JasperViewer(j, false);
+             System.out.println("entra8");
             jv.setTitle("Reporte Importe Fijo");
+             System.out.println("entra9");
             jv.setVisible(true);
-            
+           
         }
         catch(Exception e)
         {
